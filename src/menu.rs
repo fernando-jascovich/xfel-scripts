5af36579019f0e73fe31e7b55c5759147eebdf2a
Fernando Jascovich <fernando.ej@gmail.com>
2022-11-30 15:08:36 -0300

    Changing menu launch to use desktop files

--- src/menu.rs
+++ src/menu.rs
@@ -1,38 +1,60 @@
 use std::io::Write;
-use std::process::Stdio;
 use std::process::Command;
+use std::process::Stdio;
 
-
-fn bemenu_args<'a>(cmd: &'a mut Command) -> &'a mut Command {
-    cmd
-        .arg("-l").arg("5")
-        .arg("-p").arg("->")
-        .arg("-i")
-        .arg("-w")
-        .arg("-m").arg("1")
-        .arg("--fn").arg("Iosevka 33")
+fn bemenu_args() -> Vec<&'static str> {
+    return vec![
+        "-l",
+        "5",
+        "-p",
+        "->",
+        "-i",
+        "-w",
+        "-m",
+        "1",
+        "--fn",
+        "Iosevka 33",
+    ];
 }
 
 pub fn select(input_data: &str, prompt: Option<&str>) -> String {
-    let mut cmd = Command::new("bemenu");
-    let mut proc = bemenu_args(&mut cmd)
-        .arg("-p").arg(prompt.unwrap_or("->"))
+    let mut cmd = Command::new("bemenu")
+        .args(bemenu_args())
+        .arg("-p")
+        .arg(prompt.unwrap_or("->"))
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()
         .unwrap();
-    proc.stdin.as_mut().unwrap().write_all(
-        input_data.as_bytes()
-    ).unwrap();
-    String::from_utf8(
-        proc.wait_with_output().unwrap().stdout
-    ).unwrap().replace("\n", "")
+    cmd.stdin
+        .as_mut()
+        .unwrap()
+        .write_all(input_data.as_bytes())
+        .unwrap();
+    String::from_utf8(cmd.wait_with_output().unwrap().stdout)
+        .unwrap()
+        .replace("\n", "")
 }
 
 pub fn run() {
-    let mut cmd = Command::new("bemenu-run");
-    bemenu_args(&mut cmd)
+    let desktop_files = String::from_utf8(
+        Command::new("ls")
+            .arg("/usr/share/applications")
+            .output()
+            .expect("Failed to execute ls command")
+            .stdout,
+    ).unwrap();
+    let arr: Vec<String> = desktop_files
+        .split("\n")
+        .filter(|x| x.len() > 0)
+        .map(|x| {
+            let parts: Vec<&str> = x.split(".").collect();
+            parts.first().unwrap().to_string()
+        })
+        .collect();
+    let selected = select(&arr.join("\n"), None);
+    Command::new("gtk-launch")
+        .arg(selected)
         .output()
-        .expect("Failed to execute bemenu");
+        .expect("Failed to execute selected program");
 }
-
